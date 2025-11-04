7df544af19be1980179de5229e2d8de3675686fa
kittu2karthik <71444538+kittu2karthik@users.noreply.github.com>
2025-11-04 19:44:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+const audioMap = {};
+sounds.forEach(sound => {
+  audioMap[sound] = new Audio(`./sounds/${sound}.mp3`);
+});
+
+function stopSounds() {
+  sounds.forEach(sound => {
+    const audio = audioMap[sound];
+    audio.pause();
+    audio.currentTime = 0;
+  });
+}
+
+sounds.forEach(sound => {
+  const button = document.getElementById(sound);
+  button.addEventListener('click', () => {
+    stopSounds(); 
+    audioMap[sound].play();
+  });
+});
+
+// Stop button event listener
+document.getElementById('stop').addEventListener('click', stopSounds);
